ac16de1cf221990f5e2ebe491e3f8bb94ac976fa
AlanMati <alanmatiusevskij@email.com>
2024-05-28 00:08:43 +0300

    heyo

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,11 @@
 from src.include.PyCLink import pyclink
 import sys
 
-#get input
-#do something with it
-pyclink.send(pyclink.TYPES.CHAR_P, "Paspaudete 'enter'. Sis atsakymas atejo is pitono. Ateityje ateis is chatgpt (tikriausiai?).")
-sys.exit()
+def chatgpt():
+    pass
+
+#uzklausa = pyclink.receiveInput()
+uzklausa = "labas chatgpt"
+atsakymas = chatgpt() 
+
+pyclink.send(pyclink.TYPES.CHAR_P, atsakymas)
