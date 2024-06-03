21f6b0cc9d9b5d040bc25745e097e2bee7445d74
Gantzomara <25.elina.prialgauskaite@azuolynogimnazija.lt>
2024-06-04 00:43:21 +0200

    booooo

--- chatbot.py
+++ chatbot.py
@@ -3,11 +3,14 @@ from openai import OpenAI
 api_key = ""
 client = OpenAI(api_key=api_key)
 
+uzklausa=#cia kas rasoma i petto langeli
+
+
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo-0125",
   messages=[
     {"role": "system", "content": "Say 'egg'"}
+     
   ]
-)
-
+)ats=messages
 print(completion.choices[0].message)
