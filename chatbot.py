823d00fd4c7605a938f6f41a3de892c7a53afe97
AlanMati <alanmatiusevskij@email.com>
2024-06-04 01:54:42 +0300

    Chatgpt stone age

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,27 @@
 from openai import OpenAI
+from src.include.PyCLink import pyclink
 
 api_key = ""
 client = OpenAI(api_key=api_key)
 
-uzklausa=#cia kas rasoma i petto langeli
+def CHATGPT_UZKLAUSA(uzklausa):
+  inp = "If possible, keep the answer on the short side: " + uzklausa
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+      {"role": "user", "content": inp}
+    ]
+  )
+  return completion.choices[0].message.content
 
+atsakymas = None
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo-0125",
-  messages=[
-    {"role": "system", "content": "Say 'egg'"}
-     
-  ]
-)ats=messages
-print(completion.choices[0].message)
+if pyclink.checkForArgs() == False:
+  print("Launched without arguments!\nQuit or type your question.")
+  inputas = input()
+  atsakymas = CHATGPT_UZKLAUSA(inputas)
+  print(atsakymas)
+  input()
+else:
+  atsakymas = CHATGPT_UZKLAUSA(pyclink.receiveInput()) 
+  pyclink.send(pyclink.TYPES.CHAR_P, atsakymas)
